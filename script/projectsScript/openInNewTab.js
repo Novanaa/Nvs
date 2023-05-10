bd64d56090e8c5f76acced6043fcc35a7236c4b0
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-05-09 20:40:08 -0700

    update

---
+++ script/projectsScript/openInNewTab.js
@@ -0,0 +1,12 @@
+// projectsScript
+// Open In New Tab
+
+// anotherButton
+function anotherBtn() {
+  window.open("https://github.com/ItsNvaa/Hello-World", "_blank");
+}
+
+// projectsBtn
+function projectsBtn() {
+  window.open("https://github.com/ItsNvaa/CSS-Native", "_blank");
+}
