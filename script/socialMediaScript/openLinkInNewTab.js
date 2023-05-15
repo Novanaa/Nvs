a1e565af65790d624d2330f2000b79158feeaf7a
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-05-15 05:09:59 -0700

    pre-release

---
+++ script/socialMediaScript/openLinkInNewTab.js
@@ -0,0 +1,64 @@
+// buttonLink
+function mainBtn() {
+  window.open(
+    "https://www.instagram.com/thenavyowll/?igshid=ZDdkNTZiNTM",
+    "_blank"
+  );
+}
+
+// iconsLink
+// instagramIcons
+function mainIconsTwo() {
+  window.open(
+    "https://www.instagram.com/thenavyowll/?igshid=ZDdkNTZiNTM",
+    "_blank"
+  );
+}
+// facebookIcons
+function mainIconsThree() {
+  window.open(
+    "https://www.facebook.com/profile.php?id=100077631759181",
+    "_blank"
+  );
+}
+// discordIcons
+function mainIconsOne() {
+  window.open;
+}
+
+// menuSliderOnClick
+// ins
+const insMenuImages = () => {
+  window.open(
+    "https://www.instagram.com/thenavyowll/?igshid=ZDdkNTZiNTM",
+    "_blank"
+  );
+};
+
+// fb
+const fbMenuImages = () => {
+  window.open(
+    "https://www.facebook.com/profile.php?id=100077631759181",
+    "_blank"
+  );
+};
+
+// git
+const gitMenuImages = () => {
+  window.open("https://github.com/ItsNvaa", "_blank");
+};
+
+// tw
+const twMenuImages = () => {
+  window.open("https://google.com");
+};
+
+// menuOnClickLink
+// twitterIcons
+const menuIconsOne = () => {
+  window.open("https://google.com");
+};
+// whatsappIcons
+const menuIconsTwo = () => {
+  window.open("https://google.com");
+};
